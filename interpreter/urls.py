b581ee36c86c49164dcaaf4ed78dc4e581568d31
Katherinne Axpuac <kaxpuac@gmail.com>
2026-08-15 14:07:34 -0600

    recibiendo codigo fuente de prueba

--- interpreter/urls.py
+++ interpreter/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("health/", views.health_check, name="health_check"),
+    path("health/", views.health_check, name="health_check"), # ruta para la verifc de salud de la API
+    path("analyze/", views.analyze_code, name="analyze_code"), # ruta para analizar el código OxigenScript
 ]
--- interpreter/views.py
+++ interpreter/views.py
@@ -1,5 +1,7 @@
-from django.http import JsonResponse
+import json
 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def health_check(request):
     """
@@ -12,4 +14,67 @@ def health_check(request):
             "message": "La API de OxigenScript está funcionando correctamente.",
         }
     )
+
 # JsonResponse convierte el diccionario de Python en una respuesta JSON.
+
+@csrf_exempt
+def analyze_code(request):
+    """
+    recibe codigo fuente de oxigenscript en formato json
+    """
+
+    # solo se permite utilizar el metodo post
+    if request.method != "POST":
+        return JsonResponse(
+            {
+                "success": False,
+                "message": "Este endpoint solamente acepta solicitudes POST.",
+            },
+            status=405,
+        )
+
+    try:
+        # convierte el cuerpo de la solicitud en un diccionario de python
+        data = json.loads(request.body)
+
+    except (json.JSONDecodeError, UnicodeDecodeError):
+        return JsonResponse(
+            {
+                "success": False,
+                "message": "El cuerpo de la solicitud no contiene un JSON válido.",
+            },
+            status=400,
+        )
+
+    # obtiene la propiedad code del json recibido
+    code = data.get("code")
+
+    # comprueba que code exista y que sea una cadena
+    if not isinstance(code, str):
+        return JsonResponse(
+            {
+                "success": False,
+                "message": "La propiedad 'code' es obligatoria y debe ser una cadena.",
+            },
+            status=400,
+        )
+
+    # impide analizar codigo vacio o formado solamente por espacios
+    if not code.strip():
+        return JsonResponse(
+            {
+                "success": False,
+                "message": "El código fuente no puede estar vacío.",
+            },
+            status=400,
+        )
+
+    # respuesta temporal mientras se construye el analizador
+    return JsonResponse(
+        {
+            "success": True,
+            "message": "El código fue recibido correctamente.",
+            "code": code,
+        },
+        status=200,
+    )
